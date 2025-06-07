8d6182f8392a0016c30b542f09066e10a0ae4db2
Oleh Bashtovyi <olehbashtovyi@gmail.com>
2025-06-07 18:39:11 +0300

    add UpdateTodoItemStatusCommand

---
+++ src/TodoApp.Application/Features/Commands/UpdateTodoItemStatus/UpdateTodoItemStatusCommand.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using MediatR;
+using TodoApp.Application.DTOs;
+using TodoApp.Domain.Enums;
+
+namespace TodoApp.Application.Features.Commands.UpdateTodoItemStatus;
+
+public record UpdateTodoItemStatusCommand(Guid Id, TodoStatus Status) : IRequest<TodoDto>;
+
+public class UpdateTodoItemStatusCommandValidator : AbstractValidator<UpdateTodoItemStatusCommand>
+{
+    public UpdateTodoItemStatusCommandValidator()
+    {
+        RuleFor(x => x.Status)
+            .IsInEnum().WithMessage("Status must be a valid TodoStatus value.");
+    }
+}
